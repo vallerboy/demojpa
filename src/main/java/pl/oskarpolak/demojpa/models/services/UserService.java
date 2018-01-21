8803d402a2619ff799ecf9e502c8bd66233913bc
Oskar Polak <oskarpolak@vp.pl>
2018-01-21 16:06:03 +0100

    asd

--- src/main/java/pl/oskarpolak/demojpa/models/services/UserService.java
+++ src/main/java/pl/oskarpolak/demojpa/models/services/UserService.java
@@ -1,9 +1,12 @@
 package pl.oskarpolak.demojpa.models.services;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 //@Controller/Serivce/Component/Bean/Configuration/Repository
 @Service
+@Scope(scopeName = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserService {
 
     private String text;
